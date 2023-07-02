5c3fee00bdfa649f7606f0be38c77ca12d66d53e
unknown <erik17242@gmail.com>
2023-07-02 19:36:13 +0300

    Bug fixes

--- src/components/App.js
+++ src/components/App.js
@@ -117,7 +117,7 @@ function App() {
       try {
         setIsLoading(true);
         const res = await fetch(
-          `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API_KEY}`,
+          `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API_KEY}`,
           {
             signal: abortController.signal,
           }
